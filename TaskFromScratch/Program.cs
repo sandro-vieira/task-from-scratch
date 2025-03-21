82595d8b1c73caecfecddbbcd4d0b898aa873170
sandro.vieira <sandro.vieira@ambevtech.com.br>
2025-03-21 17:20:03 -0300

    feat: initial commit - Task.Run()

---
+++ TaskFromScratch/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using TaskFromScratch;
+
+Console.WriteLine($"Current Thread Id: {Thread.CurrentThread.ManagedThreadId}");
+
+ScratchTask.Run(() => Console.WriteLine($"Current Scratch Thread Id: {Thread.CurrentThread.ManagedThreadId}"));
+
+Console.ReadLine();
